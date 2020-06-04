2b14c7e3d281bbe4c2f97dd4182f078c9d3d1112
Veronika <Rudanika26@gmail.com>
2020-06-04 20:43:41 +0300

    second change

--- app.js
+++ app.js
@@ -9,25 +9,24 @@ let word = ""
     word += "_"
   }
 
-document.getElementById("try").innerHTML = attempts
+document.getElementById("try").innerHTML = attempts //hydhffhfhfhu
 document.getElementById("input").value = word
 
 
 function showLetter(clicked_id){
   let letter = document.getElementById(clicked_id).innerHTML;
   if (secretWord.i  >= 0){
-      let cutWordStart = word.slice(0,secretWord.indexOf(letter));                   //1
-      let cutWordEnd = word.slice(secretWord.indexOf(letter)+1, word.length)         //2
+      let cutWordStart = word.slice(0,secretWord.indexOf(letter));
+      let cutWordEnd = word.slice(secretWord.indexOf(letter)+1, word.length)
       word = cutWordStart + letter + cutWordEnd                                      //3
       document.getElementById("input").value = word
         } else {
             attempts--
-            document.getElementById("try").innerHTML = attempts
+            document.getElementById("try").innerHTML = attempts   //rghurhuhurgrhg
       }
       changeColor(clicked_id);
 }
 
 function changeColor(clicked_id){
     let button = document.getElementById(clicked_id);
-    button.style.background= '#DC143C';
   }
